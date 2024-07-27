945f1e3d42e495acb2a68b277cef09883b695301
abrish273 <tilahuna13@gmail.com>
2024-07-27 11:50:45 +0300

    second commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv");
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/user.routes");
@@ -7,8 +7,9 @@ const app = express();
 
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/auth_db", {
+// mongoose.connect("mongodb://localhost:27017/auth_db", {
+console.log("process.env.DB_URL", process.env.DB_URL);
+mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
